9f6d9583da21bf54ca4bd40795700c2ae29af985
Alex Bogatov <bogatov.alexandr@yandex.ru>
2022-10-17 06:35:46 +0300

    "remname"

---
+++ dictionary/person.py
@@ -0,0 +1,10 @@
+person={'Name':'Ford Perfect',
+        'Gender':'Male',
+        'Occupation':'Researcher',
+        'Home Planet':'Betelgeuse Seven'}
+print(person['Gender'])
+print(person['Name'])
+print(person['Occupation'])
+print(person['Home Planet'])
+person['Age']=33
+print(person)
