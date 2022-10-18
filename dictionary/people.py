d036d6f2d749745b61ba001819bdecee2d0b8ec0
Alex Bogatov <bogatov.alexandr@yandex.ru>
2022-10-18 07:16:35 +0300

    "use dict in dict"

---
+++ dictionary/people.py
@@ -0,0 +1,8 @@
+people={}
+people['Ford']={'Name':'Ford Perfect','Gender':'Male','Occupation':'Researcher','Home Planet':'Betelgeuse Seven'}
+people['Artur']={'Name':'Artur Dent','Gender':'Male','Occupation':'Sandwich Maker','Home Planet':'Earth'}
+people['Trillian']={'Name':'Tricia McMillan','Gender':'Female','Occupation':'Matematician','Home Planet':'Betelgeuse Seven'}
+people['Robot']={'Name':'Marvin','Gender':'Male','Occupation':'Paranoid Android','Home Planet':'Unknown'}
+import pprint
+
+print(people['Artur']['Occupation'])
