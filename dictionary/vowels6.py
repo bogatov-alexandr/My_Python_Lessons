d75dc57ff8589e6bcc53a8ee9ca107698dde6688
Alex Bogatov <bogatov.alexandr@yandex.ru>
2022-10-18 05:56:42 +0300

    "use setdefault"

--- dictionary/vowels6.py
+++ dictionary/vowels6.py
@@ -4,6 +4,7 @@ found = {}
 
 for letter in word:
     if letter in vovels:
+        found.setdefault(letter, 0)
         found[letter] += 1
 for k, v in sorted(found.items()):
     print(k, 'was found', v, 'time(s).')
