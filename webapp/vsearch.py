75f2c1022722c4962ba94fadf8d79323de728fc0
Alex Bogatov <bogatov.alexandr@yandex.ru>
2022-10-25 07:28:54 +0300

    webapp add

--- webapp/vsearch.py
+++ webapp/vsearch.py
@@ -3,7 +3,10 @@ def search4vowels(phrase: str) -> set:
     vowels = set('aeiou')
     return print(vowels.intersection(set(phrase)))
 
+def search4letters(phrase: str, letters:str='aeiou') -> set:
+    """"Возвращает множество букв из "letters", найденных в указанной фразе."""
+    return print(set(letters).intersection(set(phrase)))
 
-search4vowels('hitch-hiker')
-search4vowels('galaxy')
-search4vowels('sky')
+
+
+
